d613798d22a6727862f1e09a6a00d4a69bf615c5
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-18 20:26:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,9 @@
-# Your code goes here!
-
 class Dog
-  def name=(new_name)
-    @dog_name = new_name
+  def name=(dog_name)
+    this_dogs_name = dog_name
   end
-  
+ 
   def name
-    @dog_name
+    this_dogs_name
   end
-  
 end
